67eb651e8ff306011b65e22bcc5cf36167294cbb
jacksonk <jacksonk@bcr.co.za>
2025-06-14 08:52:18 +0200

    return status code "200" if survey was created/saved successfully

--- server/index.js
+++ server/index.js
@@ -105,7 +105,7 @@ app.post('/api/surveys', (req, res) => {
       listenToRadioRating
     );
 
-    return res.status(201).json({
+    return res.status(200).json({
       message: 'Survey submitted successfully',
       id: result.lastInsertRowid
     });
